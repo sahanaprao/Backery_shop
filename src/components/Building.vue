d73ac13d013160e8d3c743ea96e24cfeb3c3ff1e
sahanaprao <iamsahana.p@gmail.com>
2021-03-03 16:03:01 +0100

    implemented first feature

--- src/components/Building.vue
+++ src/components/Building.vue
@@ -2,16 +2,25 @@
     <div class="row">
     <div class="col-xs-6 col-md-4"></div>
     <div class="col-xs-6 col-md-4">
-        <div class="sun"></div>
-        <div class="roof"></div>
-        <div class="wall">
-            <div class="window left"></div>
-            <div class="window right"></div>
-            <div class="door">
-                <div class="locker"></div>
+        <div class="sun" @click="themeChange(isDarkTheme = !isDarkTheme)"></div>
+        <!-- <div class="backery"> -->
+            <div class="roof"></div>
+            <div class="backery-name">
+                <span>COOKIE BACKERY</span>
             </div>
-            <div class="mailBox"></div>
-        </div>
+            <div class="wall">
+                <div class="window left" v-show="!isCookie.left" @mouseover="transformCookie(true,'left')"></div>
+                <img src="images/cookie.jfif" alt="cookie" class="window left" v-show="isCookie.left" @mouseout="transformCookie(false,'left')"/>
+
+                <div class="window right" v-show="!isCookie.right" @mouseover="transformCookie(true,'right')" ></div>
+                <img src="images/cookie.jfif" alt="cookie" class="window right" v-show="isCookie.right" @mouseout="transformCookie(false,'right')"/>
+
+                <div class="door">
+                    <div class="locker"></div>
+                </div>
+                <div class="mail-box"></div>
+            </div>
+        <!-- </div> -->
     </div>
     <div class="col-xs-6 col-md-4"></div>
 
@@ -22,6 +31,22 @@
 <script>
 export default {
     name: 'building',
+    data:  function() {
+		return {
+        isCookie: {
+            left : false,
+            right : false
+            }
+        }
+    },
+    methods: {
+        themeChange:  function (isDarkTheme) {
+        document.documentElement.setAttribute('data-theme', isDarkTheme?'dark':'light');
+        },
+        transformCookie: function (isCookie,side) {
+            this.isCookie[side] = isCookie;
+        }
+    }
 }
 </script>
 
@@ -30,20 +55,20 @@ export default {
     float: right;
     width: 21%;
     height: 13vh;
-    background-color: yellow;
+    background-color: rgb(255, 255, 1);
     border-radius: 50%;
 }
 .roof {
     clear: both;
     border-left: 241px solid transparent;
     border-right: 241px solid transparent;
-    border-bottom: 192px solid red;
+    border-bottom: 192px solid rgb(221, 126, 106);
 }
 
 .wall {
     width: 100%;
     height: 45vh;
-    background-color: grey;
+    background-color: rgb(204, 204, 204);
     position: relative;
 }
 
@@ -51,8 +76,8 @@ export default {
     position: absolute;
     width: 19%;
     height: 13vh;
-    background-color: lightblue;
-    border: 2px solid blue;
+    background-color: rgb(207, 226, 243);
+    border: 2px solid rgb(159, 194, 226);
     border-radius: 50%;
 }
 
@@ -69,7 +94,7 @@ export default {
 .door {
     width: 28%;
     height: 35vh;
-    background-color: lightcoral;
+    background-color: rgb(246, 178, 107);
     bottom: 0;
     position: absolute;
     left: 0;
@@ -81,13 +106,26 @@ export default {
 
 }
 
-.mailBox {
+.mail-box {
     position: absolute;
     right: 40px;
     bottom: 80px;
     width: 25%;
     height: 10vh;
-    background-color: lightslategrey;
+    background-color: rgb(153, 153, 153);
 }
 
+.backery-name {
+   width: 41%;
+    height: 7vh;
+    background-color: hotpink;
+    z-index: 5;
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: -40px;
+    bottom: 0;
+    margin: auto;
+    text-align: center;
+}
 </style>
